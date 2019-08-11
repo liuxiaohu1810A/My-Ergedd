21320a87bdfdb77de42deac65e4138efcba3e33a
huojiale <482254527@qq.com>
2019-08-11 16:57:19 +0800

    tablayout

--- app/src/main/java/com/example/myergedd/MainActivity.java
+++ app/src/main/java/com/example/myergedd/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.myergedd;
 
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewStub;
---
+++ app/src/main/java/com/example/myergedd/fragment/chosen/ChosenFragment.java
@@ -0,0 +1,20 @@
+package com.example.myergedd.fragment.chosen;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myergedd.R;
+import com.example.myergedd.base.SimpleFragment;
+
+public class ChosenFragment extends SimpleFragment {
+
+    @Override
+    protected int getLayoutID() {
+        return R.layout.fragment_chosen;
+    }
+}
